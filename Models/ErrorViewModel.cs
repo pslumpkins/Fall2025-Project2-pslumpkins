1a8396e8a9b8c0d939e8711c9c30f783d2e8e173
pslumpkins <pslumpkins@crimson.ua.edu>
2025-09-26 12:42:31 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project2_pslumpkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
